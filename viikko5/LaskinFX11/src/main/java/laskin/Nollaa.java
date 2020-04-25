f01b007c4007214fcd25b7a9761a6e5a29efcc9e
jkukko <joonas.kukkonen@gmail.com>
2020-04-25 19:21:13 +0300

    Week 5 Exercise 4

---
+++ viikko5/LaskinFX11/src/main/java/laskin/Nollaa.java
@@ -0,0 +1,23 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Nollaa extends Komento {
+
+    public Nollaa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        sovellus.nollaa();
+        tuloskentta.setText(Integer.toString(sovellus.tulos()));
+    }
+
+    @Override
+    public void peru() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
